70bdfb492ce72468d62f6b629c81ed6fb3b6c658
mitchellmendoza <mitchm@stanford.edu>
2016-08-30 13:40:33 -0700

    updated reverse.py
    Deleted some sanity checks for my use, deleted semi colons that I habitually added.  Elaborated on my string reversal technique.

--- reverse.py
+++ reverse.py
@@ -1,3 +1,4 @@
+#uses the requests library 
 import requests
 
 payload = {'token': 'cac21dc164e91217722bb4a3da2320f4'}
@@ -5,14 +6,11 @@ payload = {'token': 'cac21dc164e91217722bb4a3da2320f4'}
 #run a simple http post requests to the first endpoint to receive our string to reverse
 r1 = requests.post("http://challenge.code2040.org/api/reverse", data=payload)
 
-#sanity check
-print(r1.text);
-
-#reverse the string using python magic
-reversedStr = r1.text[::-1];
+#reverse the string using extended slice syntax, and using a reverse slice. 
+reversedStr = r1.text[::-1]
 
 #add the new key value pair to our dictionary for the validation request
-payload['string'] = reversedStr;
+payload['string'] = reversedStr
 
 #execute the validation request
-r = requests.post("http://challenge.code2040.org/api/reverse/validate", data=payload)
+r2 = requests.post("http://challenge.code2040.org/api/reverse/validate", data=payload)
